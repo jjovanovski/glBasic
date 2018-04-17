ae48adf98282c1efa04d11b6c6660b44b5e6aedf
jjovanovski <jovanovskijovan97@gmail.com>
2018-04-17 23:12:36 +0200

    Added includes for glm and stb_image in the default main.cpp

--- template/main.cpp
+++ template/main.cpp
@@ -1,18 +1,22 @@
 #include <glad\glad.h>
 #include <GLFW\glfw3.h>
 
+#include <glm\glm.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include <iostream>
 
 int main(int argc, char** args) {
-
 	glfwInit();
-	
+		
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	
-	GLFWwindow* window = glfwCreateWindow(600, 600, "OpenGL", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "glBasic", nullptr, nullptr);
     if (window == nullptr) {
         std::cout << "Failed to create window.";
         return -1;
